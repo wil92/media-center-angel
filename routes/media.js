be617f124e721142e5e2267210a58043d51487d5
wil92 <wil@aleph.engineering>
2019-06-30 20:43:04 -0400

    Create video streaming endpoint and views

--- routes/media.js
+++ routes/media.js
@@ -1,10 +1,39 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
+
+const mime = require('mime-types');
 
 const router = express.Router();
 
 router.get('/media/:video_id', (req, res) => {
     const mediaTest = process.env['MEDIA_TEST_PATH'];
-    res.json({media_name: req.params['video_id']});
+    const mediaType = mime.lookup(mediaTest);
+    const stat = fs.statSync(mediaTest);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+    if (range) {
+        const parts = range.replace(/bytes=/, "").split("-");
+        const start = parseInt(parts[0], 10);
+        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+        const chunksize = (end - start) + 1;
+        const file = fs.createReadStream(mediaTest, {start, end});
+        const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': mediaType,
+        };
+        res.writeHead(206, head);
+        file.pipe(res);
+    } else {
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': mediaType,
+        };
+        res.writeHead(200, head);
+        fs.createReadStream(mediaTest).pipe(res);
+    }
 });
 
 module.exports.router = router;
