345572177633794be7d5813d8ff16a6d9be80901
Raúl C. Rivero <tumajasito@aol.com>
2019-07-09 14:04:00 -0400

    Resolving conflicts

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,13 @@
 const media = require('./media');
 const views = require('./views');
-<<<<<<< HEAD
 const api = require('./api');
-=======
 const server = require('./server');
->>>>>>> a96e79299762cebab50c74dae8224c3b7f629226
 
 const routes = [
     media,
     views,
-<<<<<<< HEAD
-    api
-=======
+    api,
     server
->>>>>>> a96e79299762cebab50c74dae8224c3b7f629226
 ];
 
 module.exports.loadRoutes = (app) => {
